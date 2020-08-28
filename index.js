b17b4996eb27034e452e985874c5cc4721126af9
Brian Brunner <brian.brunner.bb@gmail.com>
2020-08-28 12:27:17 -0600

    Add .math as an alias

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const { Plugin } = require('powercord/entities');
 
-const url_base = "https://latex.codecogs.com/png.latex?";
-const preamble = "\\dpi{300} \\fn_cs \\tiny \\color[RGB]{114, 137, 218}";
+const url_base = 'https://latex.codecogs.com/png.latex?'
+const preamble = '\\dpi{300} \\fn_cs \\tiny \\color[RGB]{114, 137, 218}'
 const command = 'latex'
 
 module.exports = class LatexToImage extends Plugin {
   startPlugin() {
     powercord.api.commands.registerCommand({
       command,
+      aliases: ['math'],
       description: 'Sends an image link to the compiled LaTeX code',
       usage: '{c} [ latex input ]',
       executor: (args) => ({
